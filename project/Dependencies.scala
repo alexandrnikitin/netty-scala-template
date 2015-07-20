7c3b17c719fc5ffdd20bcf2bb430f4bde87d0990
Alexandr Nikitin <nikitin.alexandr.a@gmail.com>
2015-07-20 12:09:15 +0300

    Add config dependency

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -9,6 +9,8 @@ object Dependencies {
     val epoll = "io.netty" % "netty-transport-native-epoll" % version
   }
 
+  private val config = "com.typesafe" % "config" % "1.3.0"
+
   val microservice = dependencies(Netty.all, Netty.epoll)
 
   private def dependencies(modules: ModuleID*): Seq[Setting[_]] = Seq(libraryDependencies ++= modules)
